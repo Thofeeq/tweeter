16b84bd278edfea498b72964cfef7f0ef74ef125
Thofeeq <thofeeqm@gmail.com>
2018-09-13 17:06:14 +0000

    Addings docs and updating .gitignore

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
   // Hide the form when the page loads  
   $(".new-tweet").hide();
   
+
   //Upon clicking the compose button, activate slide toggle animation(1000ms) and focus on the textarea
   $("#compose").click(function() {
     $(".new-tweet").slideToggle(1000);
     $( "#tweet-box" ).focus();
     });
+
 
   //This event handler takes care of posting the tweet
   $( "form" ).on( "submit", function( event ) {
@@ -30,80 +32,73 @@ $(document).ready(function() {
       $("#error-tooLong").hide();
       } else {
       $("#error-empty").hide();
-      $("#error-tooLong").hide();
+      $("#error-tooLong").h
+      ide();
       $.post( "/tweets/", $( this ).serialize(),function(){
         //Loading the tweets once the tweet has been posted
         (loadTweets());
         //Empty the text in form once the post has been completed.
-        $('#tweet-box').val('');
+        $("#tweet-box").val("");
         });
       }
     });
-////////////////
 
-////////////////////////////////////////////
-    function loadTweets(){
-      var arr = [];
-      $.ajax('/tweets/', { method: 'GET' })
-    .then(function (test) {
-      renderTweets(test);
-     
+
+  //Makes an AJAX get request to "/tweets", then renders all the tweets
+  function loadTweets(){
+    $.ajax("/tweets/", { method: "GET" })
+    .then(function (tweet_db) {
+      renderTweets(tweet_db);     
     });
-  
-    }
+  }
 
-
-/////////////////////////////////////////////////////////////////////
-
-function createTweetElement(tweetObj){
-  let newTweet = $("<article>").html(`<header>
-  <img src="${$("<p>").text(tweetObj.user.avatars.small).html()}">
-  <h3>${$("<h3>").text(tweetObj.user.name).html()}</h3>
-  <p>${$("<p>").text(tweetObj.user.handle).html()}</p>
-  </header>
-  <p>${$("<p>").text(tweetObj.content.text).html()}</p>
-  <hr>
-  <footer>
-  <p>${$("<p>").text(Date(tweetObj.createdAt)).html()}</p>
-  <div class="icons">
-  <span class="glyphicon glyphicon-flag  " aria-hidden="true"></span>
-  <span class="glyphicon glyphicon-retweet " aria-hidden="true"></span>
-  <span class="glyphicon glyphicon-heart  " aria-hidden="true"></span> 
-</div>
-  </footer>
-  `).addClass("tweets");   
   
+  //This function takes data from the DB and parses it into HTML responsible for a single tweet
+  function createTweetElement(tweetObj){
+    let newTweet = $("<article>").html(
+    `<header>
+      <img src="${$("<p>").text(tweetObj.user.avatars.small).html()}">
+      <h3>${$("<h3>").text(tweetObj.user.name).html()}</h3>
+      <p>${$("<p>").text(tweetObj.user.handle).html()}</p>
+    </header>
+    <p>${$("<p>").text(tweetObj.content.text).html()}</p>
+    <hr>
+    <footer>
+      <p>${$("<p>").text(Date(tweetObj.createdAt)).html()}</p>
+      <div class="icons">
+        <span class="glyphicon glyphicon-flag  " aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-retweet " aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-heart  " aria-hidden="true"></span> 
+      </div>
+    </footer>
+    `).addClass("tweets");   
   
-  return newTweet;
+    return newTweet;
 }
 
-//////////////////////////////////////////
-function renderTweets(tweets) {
-  // loops through tweets
-  tweets.forEach(function(tweet){
-   var $tweet = createTweetElement(tweet);
+  //This will render tweets given a list of user objects
+  function renderTweets(tweets) {
+    // loops through tweets
+    tweets.forEach(function(tweet){
+      //render the html for each tweet
+      var $tweet = createTweetElement(tweet);
+      //prepend the rended html to the container
+      $('.tweet-container').prepend($tweet);
 
-   $('.tweet-container').prepend($tweet);
-   $('.tweets').mouseenter(function(event){
-    console.log("here");
-    $(this).find(".icons").addClass("hover-true"); 
-    $(this).find("header").addClass("hover-true-bord");
-    $(this).find(".tweets").addClass("hover-true-body-bord");
-    $(this).find(".tweets img").addClass("hover-true-image");
-  }).mouseleave(function(){
-    $(this).find(".icons").removeClass("hover-true");
-    $(this).find("header").removeClass("hover-true-bord")
-    $(this).find(".tweets").removeClass("hover-true-body-bord");
-    $(this).find( ".tweets img").removeClass("hover-true-image");
-  });
-  })
-}
-
-function escape1(str) {
-  var article = document.createElement('article');
-  article.appendChild(document.createTextNode(str));
-  return article.innerHTML;
-}
-
-loadTweets();
+      //this takes of hover animation ( used JQuery event instead of CSS pseudo class)
+      $('.tweets').mouseenter(function(event){
+        $(this).find(".icons").addClass("hover-true"); 
+        $(this).find("header").addClass("hover-true-bord");
+        $(this).find(".tweets").addClass("hover-true-body-bord");
+        $(this).find(".tweets img").addClass("hover-true-image");
+      }).mouseleave(function(){
+        $(this).find(".icons").removeClass("hover-true");
+        $(this).find("header").removeClass("hover-true-bord")
+        $(this).find(".tweets").removeClass("hover-true-body-bord");
+        $(this).find( ".tweets img").removeClass("hover-true-image");
+      });
+    })
+  }
+  //load all stored tweets as soon page finishes loading 
+  loadTweets();
   });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,15 @@
+//This script will take care of the char counter at the corner of tweet text box
 $(document).ready(function() {
   var maxChars = 140;
-  $(".new-tweet textarea").keyup(function(event){
+  //listens for "keyup", if event fired, then update the counter accordingly
+  $(".new-tweet textarea").keyup(function() {
     var currentNum = maxChars - $(this).val().length;
-    if($(this).val().length > 140){
+    //takes care of the styling [turns red if # of chars is over 140]
+    if($(this).val().length > 140) {
       $(".counter").html(currentNum).css({color:"red"});
-    }else{
+    } else{
       $(".counter").html(currentNum).css({color:"#244751"});
     }
   });
-  });
+});
 
